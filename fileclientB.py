3f0ed5c3b45df18a04eedf523e1da20ea71d94f1
sudheer1995 <sudheerachary115@gmail.com>
2017-03-25 04:53:48 +0530

    added client & server files

---
+++ fileclientB.py
@@ -0,0 +1,288 @@
+import os
+import socket
+import signal
+import time
+from socket import *
+
+class AlarmException(Exception):
+    pass
+
+def alarmHandler(signum, frame):
+    raise AlarmException
+
+def nonBlockingRawInput ( prompt = "", timeout = 10 ):
+    signal.signal ( signal.SIGALRM, alarmHandler )
+    signal.alarm ( timeout )
+    try:
+        text = raw_input ( prompt )
+        signal.alarm ( 0 )
+        return text
+    except AlarmException:
+        print "\nSynchronize Continuing..."
+    signal.signal ( signal.SIGALRM, signal.SIG_IGN )
+    return "E"
+
+def longList ( clientSocket ):
+	data = clientSocket.recv ( 4096 )
+	print data
+
+def shortList ( clientSocket ):
+	data = clientSocket.recv ( 4096 )
+	data = data.split ( "\n" )
+	for file in data:
+		print file
+
+def regex ( clientSocket ):
+	data = clientSocket.recv ( 4096 )
+	print data
+
+def verify ( clientSocket ):
+	data = clientSocket.recv ( 4096 )
+	print data
+
+def checkall ( clientSocket ):
+	data = clientSocket.recv ( 4096 )
+	print data
+
+def tcpDownload ( clientUDPSocket, clientSocket ):
+	recievingFile, address = clientUDPSocket.recvfrom ( 1024 )
+	if recievingFile == "directory":
+		directory, address = clientUDPSocket.recvfrom ( 1024 )
+		if not os.path.exists ( directory ): 
+			os.mkdir ( directory )
+		permission, address = clientUDPSocket.recvfrom ( 1024 )
+		os.chmod ( directory, int ( permission, 8 ) ) 
+		os.chdir ( directory )
+		files, address = clientUDPSocket.recvfrom ( 1024 )
+		for i in range ( int ( files ) ):
+			tcpDownload ( clientUDPSocket, clientSocket )
+		os.chdir ( ".." )
+	elif recievingFile == "file":
+		filename, address = clientUDPSocket.recvfrom ( 1024 )
+		permission, address = clientUDPSocket.recvfrom ( 1024 )
+		if os.path.exists ( filename ):
+			clientSocket.sendto ( "exists", address )
+			cloudHash, address = clientUDPSocket.recvfrom ( 1024 )
+			command = "md5sum " + str ( filename )
+			output = os.popen ( command ).read ( )
+			output = output.split ( )
+			localHash = output[0]
+			if not cloudHash == localHash:
+				clientUDPSocket.sendto ( "continue", address )
+				localFileTime = os.path.getmtime ( filename )
+				cloudFileTime, address = clientUDPSocket.recvfrom ( 1024 )
+				cloudFileTime = float ( cloudFileTime )
+				if localFileTime > cloudFileTime:
+					# upload
+					clientUDPSocket.sendto ( "upload", address )
+					tcpFileUpload ( clientSocket, filename )
+				else:
+					# download
+					clientUDPSocket.sendto ( "download", address )
+					tcpFileDownload ( clientSocket, filename )
+			else:
+				clientUDPSocket.sendto ( "stop", address )
+		else:
+			clientSocket.send ( "notexists" )
+			tcpFileDownload ( clientSocket, filename )
+		os.chmod ( filename, int ( permission, 8 ) )
+	else:
+		print "Requested File Not Found"
+
+def tcpFileDownload ( clientSocket, filename ):
+
+	with open( filename, "wb" ) as file:
+	    while True:
+	        data = clientSocket.recv ( 1024 )
+	        if data == "Done":
+	            break
+	        file.write ( data )
+	file.close ( )
+
+def udpDownload ( clientUDPSocket ):
+	recievingFile, address = clientUDPSocket.recvfrom ( 1024 )
+	if recievingFile == "directory":
+		directory, address = clientUDPSocket.recvfrom ( 1024 )
+		if not os.path.exists ( directory ): 
+			os.mkdir ( directory )
+		permission, address = clientUDPSocket.recvfrom ( 1024 )
+		os.chmod ( directory, int ( permission, 8 ) ) 
+		os.chdir ( directory )
+		files, address = clientUDPSocket.recvfrom ( 1024 )
+		for i in range ( int ( files ) ):
+			udpDownload ( clientUDPSocket )
+		os.chdir ( ".." )
+	elif recievingFile == "file":
+		filename, address = clientUDPSocket.recvfrom ( 1024 )
+		permission, address = clientUDPSocket.recvfrom ( 1024 )
+		if os.path.exists ( filename ):
+			clientUDPSocket.sendto ( "exists", address ) 
+			cloudHash, address = clientUDPSocket.recvfrom ( 1024 )
+			command = "md5sum " + str ( filename )
+			output = os.popen ( command ).read ( )
+			output = output.split ( )
+			localHash = output[0]
+			if not cloudHash == localHash:
+				clientUDPSocket.sendto ( "continue", address )
+				localFileTime = os.path.getmtime ( filename )
+				cloudFileTime, address = clientUDPSocket.recvfrom ( 1024 )
+				cloudFileTime = float ( cloudFileTime )
+				if localFileTime > cloudFileTime:
+					# upload
+					clientUDPSocket.sendto ( "upload", address )
+					udpFileUpload ( clientUDPSocket, filename, address )
+				else:
+					# download
+					clientUDPSocket.sendto ( "download", address )
+					udpFileDownload ( clientUDPSocket, filename )
+			else:
+				clientUDPSocket.sendto ( "stop", address )
+		else:
+			clientUDPSocket.sendto ( "notexists", address )
+			udpFileDownload ( clientUDPSocket, filename )
+		os.chmod ( filename, int ( permission, 8 ) )
+	else:
+		print "Requested File Not Found"
+
+def udpFileDownload ( clientUDPSocket, filename ):
+	try:
+	    file = open ( filename, "wb+")
+	except:
+	    print "Error: File Not Found"
+	    return
+	
+	while True:
+		data, addr = clientUDPSocket.recvfrom ( 1024 )
+		if data == "done":
+			break
+		file.write ( data )
+	file.close ( )
+
+#upload
+
+def tcpFileUpload ( clientSocket, filename):
+	try:
+		file = open ( filename, "rb" )
+		byte = file.read ( 1024 )
+		while byte:
+			clientSocket.send ( byte )
+			byte = file.read ( 1024 )
+		time.sleep ( 1 )
+		clientSocket.send ( "Done" )
+		file.close ( )
+	except:
+		print "Error"
+
+def udpFileUpload ( clientUDPSocket, filename, addr ):
+	try:
+		file = open ( filename, "rb" )
+		byte = file.read ( 1024 )
+		while byte:
+			clientUDPSocket.sendto ( byte, addr )
+			# data, addr = clientUDPSocket.recvfrom ( 1024 )
+			byte = file.read ( 1024 )
+		clientUDPSocket.sendto ( "done", addr )
+		file.close ( )
+	except:
+        		print "Error"
+
+#upload
+
+def synchronize ( clientUDPSocket, address ):
+	os.chdir ( "syncShared" )
+	files, address = clientUDPSocket.recvfrom ( 1024 )
+	for i in range ( int ( files ) ):
+		udpDownload ( clientUDPSocket )
+	os.chdir ( ".." )
+
+if __name__ == "__main__":
+	
+	clientSocket = socket ( AF_INET, SOCK_STREAM )
+	clientUDPSocket = socket ( AF_INET, SOCK_DGRAM )
+	port = 6003
+	udpPort = 6004
+	host = gethostname ( )
+	addr = ( host, udpPort )
+	clientUDPSocket.bind ( ( host, udpPort ) )
+	clientSocket.connect ( ( host, port ) )
+	log = open ( "Client-Server.log", "a+" )
+	log.write ( "-----------------------------------------------------------------------------------------\n" )
+	iteration = 0
+	print "***************** Shared Directory Client *****************"
+
+	while True:
+		iteration += 1
+		choice = nonBlockingRawInput ( ">>> Enter Command Line ( Y or N ): ")
+		clientSocket.send ( choice )
+		
+		if choice == "Y":
+			argument = raw_input ( ">>> ")
+			clientSocket.send ( argument )
+			detail = str ( "Shared Client : |" ) + str ( iteration ) + " : |" + str ( argument ) + " . \n"
+			log.write ( detail )
+
+		elif choice == "N" or choice == "E":
+			synchronize ( clientUDPSocket, addr )
+			detail = str ( "Shared Client : |" ) + str ( iteration ) + " : |" + str ( "synchronize" ) + " . \n"
+			log.write ( detail )
+			continue
+
+		else:
+			message = "Error: Not a Proper input ! "
+			detail = str ( "Shared Client : |" ) + str ( iteration ) + " : |" + str ( message ) + " . \n"
+			log.write ( detail )
+			print message
+			continue
+
+		if argument == "close":
+			print "***************** Shared Directory Sync Closed *****************"
+			clientSocket.close ( )
+			clientUDPSocket.close ( )
+			log.write ( "-----------------------------------------------------------------------------------------\n" )
+			log.close ( )
+			break
+
+		argument = argument.split ( )
+
+		if argument[0] == "index":
+	
+			if argument[1] == "longlist":
+				longList ( clientSocket )
+	
+			elif argument[1] == "shortlist":
+				shortList ( clientSocket )
+
+			elif argument[1] == "regex":
+				regex ( clientSocket )
+
+			else:
+				print "Error: Command Not Found"
+		
+		elif argument[0] == "hash":
+
+			if argument[1] == "verify":
+				verify ( clientSocket )
+
+			elif argument[1] == "checkall":
+				checkall ( clientSocket )
+			
+			else:
+				print "Error: Command Not Found"
+
+		elif argument[0] == "download":
+		
+			if argument[1] == "TCP":
+				os.chdir ( "syncShared" )
+				tcpDownload ( clientUDPSocket, clientSocket )
+				os.chdir ( "..")
+
+			elif argument[1] == "UDP":
+				os.chdir ( "syncShared" )
+				udpDownload ( clientUDPSocket )
+				os.chdir ( ".." )
+
+			else:
+				print "Error: Command Not Found"
+
+		else:
+			print "Error: Command Not Found"
---
+++ fileserverB.py
@@ -0,0 +1,282 @@
+import os
+import socket
+import time
+from socket import *
+
+def shortList ( clientSocket, timestamp1, timestamp2 ):
+	os.chdir ( "shared" )
+	# index shortlist 2015-03-20 13:20:10.888016945 +0530 2018-03-20 13:20:10.888016945 +0530
+	output = ""
+	command = "find ./ -type f -newermt '" + str ( timestamp1 ) + "' ! -newermt '" + str ( timestamp2 ) + str ( "'" )
+	files = os.popen ( command ).read ( )
+	files = files.split ( "\n" )
+	for file in files:
+		if file == '':
+			output += str ( os.popen ( "ls -l "+str ( file ) ).read ( ) )
+	clientSocket.send ( output )
+	os.chdir ( ".." )	
+
+def longList ( clientSocket ):
+	os.chdir ( "shared" )
+	command = "ls -lah"
+	output = os.popen ( command ).read ( ) 
+	clientSocket.send ( output ) 
+	os.chdir ( ".." )
+
+def regex ( clientSocket, matcher ):
+	os.chdir ( "shared" )
+	command = "ls -l " + str ( matcher )
+	output = os.popen ( command ).read ( )
+	clientSocket.send ( output )
+	os.chdir ( ".." )
+
+def verify ( clientSocket, filename ):
+	os.chdir ( "shared" )
+	command = "md5sum " + str ( filename )
+	output = os.popen ( command ).read ( )
+	clientSocket.send ( output ) 
+	os.chdir ( ".." )
+
+def checkall ( clientSocket ):
+	os.chdir ( "shared" )
+	output = ""
+	command = "ls"
+	files = os.popen ( command ).read ( )
+	files = files.split ( "\n" )
+	for file in files:
+		if not file == '' and not os.path.isdir ( file ):
+			command = "md5sum " + str ( file )
+			result = os.popen ( command ).read ( )
+			output += str ( result )
+	clientSocket.send ( output )
+	os.chdir ( ".." )
+
+def tcpDownload ( clientUDPSocket, clientSocket, filename ):
+	if os.path.exists ( filename ):
+		if os.path.isdir ( filename ):
+			clientUDPSocket.sendto ( "directory", addr )
+			clientUDPSocket.sendto ( filename, addr )
+			permissions = os.popen ( "stat -c ' %a ' " + filename ).read ( )
+			clientUDPSocket.sendto ( permissions, addr )
+			os.chdir ( filename )
+			files = os.popen ( "ls" ).read ( )
+			files = files.split ( "\n" )
+			clientUDPSocket.sendto ( str ( len ( files ) - 1 ), addr )
+			for file in files:
+				if not file == '':
+					tcpDownload ( clientUDPSocket, clientSocket, file )
+			os.chdir ( ".." )
+		else:
+			clientUDPSocket.sendto ( "file", addr )
+			clientUDPSocket.sendto ( filename, addr )
+			permissions = os.popen ( "stat -c ' %a ' " + filename ).read ( )
+			existence, address = clientSocket.recvfrom ( 1024 ) 
+			if existence == "exists":
+				command = "md5sum " + str ( filename )
+				output = os.popen ( command ).read ( )
+				output = output.split ( )
+				clientUDPSocket.sendto ( output[0], addr )
+				status, address = clientUDPSocket.recvfrom ( 1024 )
+				if status == "continue": 
+					clientUDPSocket.sendto ( str ( os.path.getmtime ( filename ) ), addr )
+					process, address = clientUDPSocket.recvfrom ( 1024 )
+					if process == "upload":
+						# upload
+						tcpFileUpload ( clientSocket, filename )
+					else:
+						# download
+						tcpFileDownload ( clientSocket, filename )
+			else:
+				tcpFileDownload ( clientSocket, filename )
+	else:
+		clientUDPSocket.sendto ( "NotFound", addr )
+		
+def tcpFileDownload ( clientSocket, filename):
+	try:
+		file = open ( filename, "rb" )
+		byte = file.read ( 1024 )
+		while byte:
+			clientSocket.send ( byte )
+			byte = file.read ( 1024 )
+		time.sleep ( 1 )
+		clientSocket.send ( "Done" )
+		file.close ( )
+	except:
+		print "Error"
+
+def udpDownload ( clientUDPSocket, filename, addr ):
+	if os.path.exists ( filename ):
+		if os.path.isdir ( filename ):
+			clientUDPSocket.sendto ( "directory", addr )
+			clientUDPSocket.sendto ( filename, addr )
+			permissions = os.popen ( "stat -c ' %a ' " + filename ).read ( )
+			clientUDPSocket.sendto ( permissions, addr )
+			os.chdir ( filename )
+			files = os.popen ( "ls" ).read ( )
+			files = files.split ( "\n" )
+			clientUDPSocket.sendto ( str ( len ( files ) - 1 ), addr )
+			for file in files:
+				if not file == '':
+					udpDownload ( clientUDPSocket, file, addr )
+			os.chdir ( ".." )
+		else:
+			clientUDPSocket.sendto ( "file", addr )
+			clientUDPSocket.sendto ( filename, addr )
+			permissions = os.popen ( "stat -c ' %a ' " + filename ).read ( )
+			clientUDPSocket.sendto ( permissions, addr )
+			existence, address = clientUDPSocket.recvfrom ( 1024 )
+			if existence == "exists":
+				command = "md5sum " + str ( filename )
+				output = os.popen ( command ).read ( )
+				output = output.split ( )
+				clientUDPSocket.sendto ( output[0], addr )
+				status, address = clientUDPSocket.recvfrom ( 1024 )
+				if status == "continue": 
+					clientUDPSocket.sendto ( str ( os.path.getmtime ( filename ) ), addr )
+					process, address = clientUDPSocket.recvfrom ( 1024 )
+					if process == "upload":
+						# upload
+						udpFileUpload ( clientUDPSocket, filename )
+					else:
+						# download
+						udpFileDownload ( clientUDPSocket, filename, addr )
+			else:
+				udpFileDownload ( clientUDPSocket, filename, addr )
+	else:
+		clientUDPSocket.sendto ( "NotFound", addr )
+
+def udpFileDownload ( clientUDPSocket, filename, addr ):
+	try:
+		file = open ( filename, "rb" )
+		byte = file.read ( 1024 )
+		while byte:
+			clientUDPSocket.sendto ( byte, addr )
+			# data, addr = clientUDPSocket.recvfrom ( 1024 )
+			byte = file.read ( 1024 )
+		clientUDPSocket.sendto ( "done", addr )
+		file.close ( )
+	except:
+        		print "Error"
+
+# upload
+
+def udpFileUpload ( clientUDPSocket, filename ):
+	try:
+	    file = open ( filename, "wb+")
+	except:
+	    # print "Error: File Not Found"
+	    return
+	
+	while True:
+		data, addr = clientUDPSocket.recvfrom ( 1024 )
+		if data == "done":
+			break
+		file.write ( data )
+	file.close ( )
+
+def tcpFileUpload ( clientSocket, filename ):
+
+	with open( filename, "wb" ) as file:
+	    while True:
+	        data = clientSocket.recv ( 1024 )
+	        if data == "Done":
+	            break
+	        file.write ( data )
+	file.close ( )
+
+# upload
+
+def synchronize ( clientUDPSocket, address ):
+	os.chdir ( "shared" )
+	files = os.popen ( "ls" ).read ( )
+	files = files.split ( '\n' )
+	clientUDPSocket.sendto ( str ( len ( files ) - 1 ), address )
+	for file in files:
+		if not file == '':
+			udpDownload ( clientUDPSocket, file, address )
+	os.chdir ( ".." )
+
+if __name__ == "__main__":
+
+	serverSocket = socket ( AF_INET, SOCK_STREAM )
+	clientUDPSocket = socket ( AF_INET, SOCK_DGRAM )
+	host = gethostname ( )
+	port = 6003
+	udpPort = 6004
+	addr = ( host, udpPort )
+	serverSocket.bind ( ( host, port ) )
+	serverSocket.setsockopt ( SOL_SOCKET, SO_REUSEADDR, 1)
+	serverSocket.listen ( 5 )
+	# print ">>> Server Listening... "
+	
+	clientSocket, clientAddress = serverSocket.accept ( )
+
+	while True:
+		choice = str ( clientSocket.recv ( 1024 ) ) 
+
+		if choice == "Y":
+			argument = clientSocket.recv ( 1024 )
+
+		elif choice == "N" or choice == "E":
+			# print ">>> Synchronizing ..... "
+			synchronize ( clientUDPSocket, addr )
+			continue
+
+		else:	
+			# print ">>> Error: Not a Proper Input !"
+			continue
+			
+		# print ">>> Serving ", argument
+		argument = argument.split ( )
+
+		if argument[0] == "index":
+			
+			if argument[1] == "longlist":
+				longList ( clientSocket )
+
+			elif argument[1] == "shortlist":
+				shortList ( clientSocket, str ( str ( argument[2] ) + str ( ' ' ) + str ( argument[3] ) +str ( ' ' ) + str ( argument[4] ) ),
+					 str ( str ( argument[5] ) + str ( ' ' ) + str ( argument[6] ) + str ( ' ' ) + str ( argument[7] ) ) )
+
+			elif argument[1] == "regex":
+				regex ( clientSocket, argument[2] )
+
+			else:
+				# print "Error: Command Not Found"
+				continue
+
+		elif argument[0] == "hash":
+			
+			if argument[1] == "verify":
+				verify ( clientSocket, argument[2] )
+		
+			elif argument[1] == "checkall":
+				checkall ( clientSocket )
+		
+			else:
+				# print "Error: Command Not Found"
+				continue
+
+		elif argument[0] == "download":
+			
+			if argument[1] == "TCP":
+				os.chdir ( "shared" )
+				tcpDownload ( clientUDPSocket, clientSocket, argument[2] )
+				os.chdir ( ".." )
+
+			elif argument[1] == "UDP":
+				os.chdir ( "shared" )
+				udpDownload ( clientUDPSocket, argument[2], addr )
+				os.chdir ( ".." )
+
+			else:
+				# print "Error: Command Not Found"
+				continue
+
+		else:
+			# print "Error: Command Not Found"
+			continue
+
+	clientSocket.close ( )
+	clientUDPSocket.close ( )
+	serverSocket.close ( )
